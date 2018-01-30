3fa613426a536cb8e5ea310c40aa8f5e7f3188bd
SeaHoonPark <tpgns0306@gmail.com>
2018-01-30 19:14:50 +0900

    remove pystargram

--- pystargram/pystargram/photos/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Photo
-from django.shortcuts import get_object_or_404
-
-def detail(request, pk):
-    photo = get_object_or_404(Photo, pk=pk)
-    context = dict()
-    context['photo'] = photo
-    return render(request, 'photos/detail.html', context)
