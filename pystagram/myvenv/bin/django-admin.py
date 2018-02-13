d621210c45c3eeb726afc9a131b2d4f1fba432a0
SeaHoonPark <tpgns0306@gmail.com>
2018-02-13 16:22:44 +0900

    pystagram

---
+++ pystagram/myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/parkseahoon/Desktop/student_developer/pystagram/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
